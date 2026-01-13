a91e61f60179d6729640f656711e1366cfab6a0b
sethg <sethg@geographika.co.uk>
2026-01-13 18:56:15 +0100

    Linting

--- docs/examples/create_network_geopandas.py
+++ docs/examples/create_network_geopandas.py
@@ -1,6 +1,7 @@
 """
 python ./docs/examples/create_network_geopandas.py
 """
+
 import geopandas as gpd
 from wayfarer import loader
 import os
@@ -8,19 +9,19 @@ import os
 
 def load_network(input_lines, output_network, overwrite=False):
 
-    lines = gpd.read_file(input_lines, layer='edges')
+    lines = gpd.read_file(input_lines, layer="edges")
     # add a unique field
     lines["oid"] = range(1, len(lines) + 1)
     lines.drop(columns="key", inplace=True)
     print(lines.head())
 
-    if not os.path.exists(output_network) or overwrite == True:
+    if not os.path.exists(output_network) or overwrite is True:
         net = loader.load_network_from_records(
             lines.to_dict("records"),
             key_field="oid",
             length_field="length",
             from_field="from",
-            to_field="to"
+            to_field="to",
         )
         loader.save_network_to_file(net, output_network)
     return loader.load_network_from_file(output_network)
